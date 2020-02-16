8af84913089d62713dd08c04ffa9b875a8f2843a
Aleksey <jerakrus@gmail.com>
2020-02-16 22:29:37 +0300

    перемещаем контроллер инструкций в апи

---
+++ app/controllers/api/instructions_controller.rb
@@ -0,0 +1,36 @@
+class Api::InstructionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    instruction = Instruction.create! instruction_params
+    if instruction
+      render json: instruction
+    else
+      render json: instruction.errors
+    end
+  end
+
+  def index
+    instructions = Instruction.all.order(created_at: :desc)
+    render json: instructions
+  end
+
+  def show
+    instruction = Instruction.find(params[:id])
+    render json: instruction
+  end
+
+  def destroy
+    instruction = Instruction.find(params[:id])
+    instruction.destroy
+    render json: { message: "Instruction with id #{params[:id]} deleted." }
+  end
+
+  private
+
+  def instruction_params
+    params.permit(:title, :instruction_type, :description, :image, :actions)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
+  namespace :api do
+    resources :instructions
+  end
+  
   root 'homepage#index'
+  get '/*path' => 'homepage#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :instructions
 end
