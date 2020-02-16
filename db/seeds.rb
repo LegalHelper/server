90d7baec78c6b3a4ba5ac7e0ba40c049f0e3fcb1
Aleksey <jerakrus@gmail.com>
2020-02-16 22:31:27 +0300

    тестовые данные для инструкций

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+9.times do |i|
+  Instruction.create(
+    title: "Instruction #{i + 1}",
+    description: '227g tub clotted cream, 25g butter, 1 tsp cornflour,100g parmesan, grated nutmeg, 250g fresh fettuccine or tagliatelle, snipped chives or chopped parsley to serve (optional)',
+    instruction_type: 'fast',
+    image: 'https://sun9-9.userapi.com/c4384/g37962418/a_86cad53d.jpg?ava=1'
+  )
+end
