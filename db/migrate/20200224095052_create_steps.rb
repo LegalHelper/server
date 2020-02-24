2e3ae7147197787dc28dd68a25ca7fa796e64a5c
Aleksey <jerakrus@gmail.com>
2020-02-24 14:08:44 +0300

    фикс миграции

--- db/migrate/20200224095052_create_steps.rb
+++ db/migrate/20200224095052_create_steps.rb
@@ -3,8 +3,8 @@ class CreateSteps < ActiveRecord::Migration[6.0]
     create_table :steps do |t|
       t.integer :parent
       t.string :title
-      t.text :description,
-      t.string :image,
+      t.text :description
+      t.string :image
       t.integer :children, array: true, default: []
       t.references :instruction
 
