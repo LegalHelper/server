f7cbefc90a31f33b200fe3b37293124b0c4fcbc1
Aleksey <jerakrus@gmail.com>
2020-03-15 01:13:21 +0300

    перенос шагов и добавление в них кнопок. и delete Callback

--- app/javascript/components/common/Step.jsx
+++ app/javascript/components/common/Step.jsx
@@ -1,7 +1,8 @@
 import React, { Component } from 'react'
-import { update_step } from '../api'
+import { update_step, delete_step } from '../../api'
+import { IoMdCreate, IoIosTrash, IoMdCheckmark ,IoMdClose } from 'react-icons/io'
 
-
+// props: step, editMode, autoSave , withButtons, saveCallBack, deleteCallBack
 export default class Step extends Component {
   constructor(props) {
     super(props)
@@ -24,17 +25,14 @@ export default class Step extends Component {
     this.setState({ step: { ...this.state.step, [event.target.name]: event.target.value } })
   }
 
-  onSubmit = () => {
-    this.update_step()
-  }
-
   onBlur = () => {
+    const { autoSave } = this.props
     const { step, editMode } = this.state
-    if (step.title.length === 0 || step.description.length === 0) return
-    this.update_step()
+    if (!autoSave || step.title.length === 0 || step.description.length === 0) return
+    this.updateStep()
   }
 
-  update_step = async() => {
+  updateStep = async() => {
     const step = this.assembleStep()
     const res = await update_step({step})
     this.props.saveCallback && this.props.saveCallback(step)
@@ -44,12 +42,64 @@ export default class Step extends Component {
     this.setState({ ...this.state, editMode: !this.state.editMode })
   }
 
+  deleteStep = async() => {
+    const { id } = this.props.step
+    const text = 'Внимание! При удалении шага, так же будут удалены все его дочерние шаги.'
+    if (confirm(text)) {
+      res = await delete_step(id)
+      this.props.deleteCallBack && this.props.deleteCallBack(id)
+    }
+  }
+
+  onSuccessEditPress = () => {
+    this.updateStep()
+    this.changeEditMode()
+  }
+
+  onCancelEditPress = () => {
+    const { title, image, description } = this.props.step
+    this.setState({
+      ...this.state,
+      editMode: !this.state.editMode,
+      step: { title, description, image: image || '' }
+    })
+  }
+
+  renderButtons = () => {
+    const { editMode } = this.state
+    if (editMode) {
+      return (
+        <div className="row justify-content-end px-3">
+          <button type="button" className="btn-sm btn-outline mx-1" onClick={this.onSuccessEditPress}>
+            <IoMdCheckmark size={24}/>
+          </button>
+          <button type="button" className="btn btn-sm custom-button mx-1" onClick={this.onCancelEditPress}>
+            <IoMdClose size={24}/>
+          </button>
+        </div>
+      )
+    } else {
+      return (
+        <div className="row justify-content-end px-3">
+          <button type="button" className="btn-sm btn-outline mx-1" onClick={this.changeEditMode}>
+            <IoMdCreate size={24}/>
+          </button>
+          <button type="button" className="btn btn-sm custom-button mx-1" onClick={this.deleteStep}>
+            <IoIosTrash size={24}/>
+          </button>
+        </div>
+      )
+    }
+  }
+
   render() {
+    const { withButtons } = this.props
     const { step } = this.state
     console.log('step', step)
     return (
       <div className="card p-2 m-2">
-        <form onSubmit={this.onSubmit}>
+        { withButtons && this.renderButtons() }
+        <form onSubmit={this.updateStep}>
           <div className="form-group">
             <label htmlFor="instructionTitle">Название шага:</label>
             <input
