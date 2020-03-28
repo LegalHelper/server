1e1ed2b1cbf0ec86c230e10055b5b5ea2845c105
Aleksey <jerakrus@gmail.com>
2020-03-28 15:43:32 +0300

    делаем show instruction

---
+++ app/javascript/components/ShowInstruction.jsx
@@ -0,0 +1,146 @@
+import React from "react"
+import { Link } from "react-router-dom"
+import InfiniteScroll from 'react-infinite-scroll-component'
+import Step from './common/Step'
+import Instruction from './common/Instruction'
+import { responsiveHeight } from '../utils'
+import produce from 'immer'
+import { get_instruction, create_step } from '../api'
+
+class ShowInstruction extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      instruction: null, //{},
+      steps: {}
+    }
+  }
+
+  componentDidMount = () => {
+    const { match: { params: { id } } } = this.props
+    this.getInstruction(id)
+  }
+
+  getInstruction = async(id) => {
+    try {
+      const { steps, instruction } = await get_instruction(id)
+      const normalizeSteps = steps.reduce((acc, step) => ({ ...acc, [step.id]: step }), {})
+      this.setState({ instruction, steps: normalizeSteps })
+    } catch (e) {
+       alert('Ошибка загрузки инструкции: ' + e.message)
+       this.props.history.push('/instructions')
+    }
+  }
+
+  deleteInstruction = (id) => async() => {
+    await delete_instruction(id)
+    this.props.history.push('/instructions')
+   }
+
+  instructionCallback = (instruction) => {
+    this.setState({instruction})
+  }
+
+  addStep = (parent_id) => async() => {
+    event.preventDefault()
+    const body = { step: { parent: parent_id } }
+    const step = await create_step(this.state.instruction.id, body)
+
+    this.setState(produce(draft => {
+      draft.steps[step.id] = step
+      if (draft.steps[parent_id]) {
+        draft.steps[parent_id].children.push(step.id)
+      }
+      return draft
+    }))
+  }
+
+  getTreeDeep = () => {
+    const { steps } = this.state
+    const get_deep = (step_ids, count = 0) => {
+      if (step_ids.length === 0) return count
+      const deep_children = step_ids.map(id => get_deep(steps[id].children, count +1))
+      return Math.max(...deep_children)
+    }
+    const rootSteps = Object.keys(steps).filter(id => steps[id].parent === null)
+    return get_deep(rootSteps)
+  }
+
+  getColumnIndices = () => {
+    const deep = this.getTreeDeep()
+    const columns = []
+    for (let i = 0; i <= deep; i += 1) { columns.push(i) }
+    return columns
+  }
+
+  collectSteps = (column) => {
+    const { steps } = this.state
+
+    const collect = (step_id, deep) => {
+      if (deep === 1) return {parent_id: step_id, children: steps[step_id].children}
+      return steps[step_id].children.map(id => collect(id, deep - 1))
+    }
+
+    const rootSteps = Object.keys(steps).filter(id => steps[id].parent === null)
+    if (column === 0) return [{parent_id: null, children: rootSteps}]
+    return rootSteps.map(id => collect(id, column)).flat(column-1)
+  }
+
+  stepSaveCallback = (step) => {
+    this.setState(produce((draft) => {
+      draft.steps[step.id] = step
+      return draft
+    }))
+  }
+
+  renderSteps = () => {
+    const columns = this.getColumnIndices()
+    return (
+      <div className="col-12 col-md-8 col-xl-9 horizontal-scroll">
+          {columns.map(this.renderStepsColumn)}
+      </div>
+    )
+  }
+
+// [ {parent_id: '', children: [children_ids]}, ...]
+  renderStepsColumn = (index) => {
+    const steps = this.collectSteps(index)
+    return (
+      <InfiniteScroll key={String(index)} className="mx-2 align-items-center justify-content-center"
+                      dataLength={steps.length} height={responsiveHeight(98)}>
+        {steps.map((step) => this.renderStepsBlock(step))}
+      </InfiniteScroll>
+    )
+  }
+
+  renderStepsBlock = ({parent_id, children}) => {
+    return (
+      <div key={parent_id} className='card my-4 align-items-center'>
+        {Boolean(parent_id) && <h2 className="font-weight-normal mb-1">{this.state.steps[parent_id].title}</h2>}
+        {this.state.instruction && (
+          <button type='button' className="btn custom-button mx-5 my-1" onClick={this.addStep(parent_id)}>
+            Добавить шаг
+          </button>
+        )}
+        {children.map((id, i) => <Step key={String(i)} step={this.state.steps[id]} withButtons
+                                         saveCallback={this.stepSaveCallback} deleteCallback={() => {}}/>)}
+      </div>
+    )
+  }
+
+  render() {
+    const { instruction } = this.state
+    return (
+      <div className="container-fluid">
+        <div className="row align-items-center">
+          {instruction && <Instruction instruction={instruction} saveCallback={this.instructionCallback} />}
+          {this.renderSteps()}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default ShowInstruction
+
+        //  <div className="overlay bg-dark position-absolute" />
--- app/javascript/routes/Index.jsx
+++ app/javascript/routes/Index.jsx
@@ -2,7 +2,7 @@ import React from "react"
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom"
 import Main from "../components/MainInstructions"
 import ListInstructions from '../components/ListInstructions'
-import Instruction from '../components/Instruction'
+import ShowInstruction from '../components/ShowInstruction'
 import CreateInstruction from '../components/CreateInstruction'
 
 export default (
@@ -10,7 +10,7 @@ export default (
     <Switch>
       <Route path="/" exact component={Main} />
       <Route path="/instructions" exact component={ListInstructions} />
-      <Route path="/instructions/:id" exact component={Instruction} />
+      <Route path="/instructions/:id" exact component={ShowInstruction} />
       <Route path="/create_instruction" exact component={CreateInstruction} />
     </Switch>
   </Router>
